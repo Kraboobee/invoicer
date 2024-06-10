81c8102467a2f9f0ad87a3c1463068bfb1d7f301
Kaamandkomposed <kaamandkomposed@gmail.com>
2024-06-10 20:14:35 +0200

    add table

--- src/components/PriceTable/PriceTable.tsx
+++ src/components/PriceTable/PriceTable.tsx
@@ -1,10 +1,28 @@
+import classes from './PriceTable.module.css';
 import { Table } from '@mantine/core';
 
 const items = [
   { description: 'Web Design and Development Service', price: 5000 },
-  { description: 'extorsion', price: 50000 },
+  { description: 'extortion', price: 50000 },
 ];
 
+const rows = items.map((item) => (
+  <Table.Tr key={item.description}>
+    <Table.Td>{item.description}</Table.Td>
+    <Table.Td className={classes.price}>R {item.price}</Table.Td>
+  </Table.Tr>
+));
+
 export function PriceTable() {
-  return <Table striped>{/* {...rows} */}</Table>;
+  return (
+    <Table striped>
+      <Table.Thead>
+        <Table.Tr>
+          <Table.Th>Description</Table.Th>
+          <Table.Th className={classes.price}>Price</Table.Th>
+        </Table.Tr>
+      </Table.Thead>
+      <Table.Tbody>{rows}</Table.Tbody>
+    </Table>
+  );
 }
